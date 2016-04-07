201a25a8cbbd17d9ab4cf8586ad609bcc366f8b8
T-SUGIUE <tsugiue@MBA-11AJ-071.local>
2016-04-07 14:12:00 +0900

    mysql-conn

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "commit done.";
 echo "<h1>Hello world</h1>";
-$mysqli = new mysqli(getenv("WATARASE_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+$mysqli = new mysqli(getenv("WATARASE_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
 if ($mysqli->connect_error) {
     exit($mysqli->connect_error);
 } else {
